b2885046fb950308bc86b5dac7e06314ee8f5066
solmin <solminkim03@naver.com>
2021-07-19 14:20:48 +0900

    화살표 함수

---
+++ javascript/test8.js
@@ -0,0 +1,22 @@
+// arrow function
+
+function add(x,y){
+    return x+y;
+}   // 함수 이름으로 만드는 방법 
+
+let add=(x,y)=>{
+    return x+y;
+}
+
+function sub(){
+    console.log('test');
+}
+
+let sub=()=>{
+    console.log('test');
+}
+
+let a=10;
+let b=20;
+console.log(`a = ${a}, b=${b}`);
+
