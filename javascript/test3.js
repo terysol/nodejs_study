c173e52fb1da3e7a10511d993c312bb83bd01fe8
solmin <solminkim03@naver.com>
2021-07-19 11:13:22 +0900

    변수 scope

--- javascript/test3.js
+++ javascript/test3.js
@@ -1,29 +1,37 @@
+// 변수 scope
+
 /* var */
-var sum=0;
-for(var i=1;i<=10;i++){
-    sum+=i;
-}
+// var sum=0;
+// for(var i=1;i<=10;i++){
+//     sum+=i;
+// }
 
-console.log("sum", sum);    // 55
-console.log("i", i)         // 11
-console.log("-----------------------------");
+// console.log("sum", sum);    // 55
+// console.log("i", i)         // 11
+// console.log("-----------------------------");
 
-/** function 함수 */
-function foo(){
-    var sum=0;      // var는 함수 단위
-    for(var i=1;i<=10;i++){
-        sum+=i;
-    }
-}
-foo();
-console.log("sum", sum);    // error
-console.log("i", i)         // error
+/** function 함수 scope*/
+// function foo(){
+//     var sum=0;      // var는 함수 단위
+//     for(var i=1;i<=10;i++){
+//         sum+=i;
+//     }
+// }
+// foo();
+// console.log("sum", sum);    // error
+// console.log("i", i)         // error
 
 
 /* let */
-let sum=0;
-for(let i=1;i<=10;i++){
-    sum+=i;
-}
-console.log("sum", sum);        // 0
-console.log("i", i)         // error
+// let sum=0;
+// for(let i=1;i<=10;i++){
+//     sum+=i;
+// }
+// console.log("sum", sum);        // 55
+// console.log("i", i)         // error
+console.log("----------------------------");
+
+/* const */
+const j=10;
+console.log(j);
+j=20;
