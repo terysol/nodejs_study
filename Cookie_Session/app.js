d8efcdffc48c26cac0117d9ade62b7a2b9c21933
solmin <solminkim03@naver.com>
2021-07-22 10:57:24 +0900

    cookie

---
+++ Cookie_Session/app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const port = 5000;
+const app=express();
+
+app.use(express.static(__dirname + "/public"));  
+app.use(express.urlencoded({extended : true}));
+
+app.set('views', './views')
+// 나는 view를 사용하겠다. 
+app.set('view engine', 'pug')       // 나는 pug를 사용
+app.locals.pretty=true;     // pug 이쁘게 만들기
+
+
+app.get("/",(req,res)=>{
+    res.send("hi node");
+})
+
+app.get('/cookie',(req,res)=>{
+    res.render("cookie_form");
+})
+app.post("/cookie",(req,res)=>{
+    let id=req.body.uid;
+    res.cookie('userId',id);
+    res.send("쿠키 설정완료");
+})
+
+app.listen(port,()=>{
+    console.log(`Connection to ${port}`);
+})
